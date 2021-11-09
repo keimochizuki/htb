4079c3d9423ab590fda545cae6ecd2e0d512f1d9
Kei Mochizuki <k31mczk-git@yahoo.co.jp>
2021-11-09 10:17:45 +0900

    Added wrapdeg and its documentation

--- R/wrapdeg.R
+++ R/wrapdeg.R
@@ -1,18 +1,56 @@
+#' Adjuster of angular values
+#'
+#' Wraps and adjusts angular values within a given range.
+#'
+#' Angular values (degrees and radians) are one of the popular parameters
+#' you need to handle during analyses,
+#' especially in visual tasks and 2-D motor tasks.
+#' [wrapdeg()] wraps the given angular values into a given range of degree
+#' like [-180, 180) or [0, 360).
+#' It allows left-open ranging, e.g., (-180, 180]
+#' when `leftopen` is `TRUE`.
+#' It also allows radian operation when `rad` is `TRUE`.
+#'
+#' @param deg Numerics. Angular values you want to adjust.
+#' @param min A numeric. The minimal value (left limit) of angular range.
+#' @param leftopen A logical. Whether to use left-open ranging.
+#'   By default `FALSE`, meaning that [min, min + 360) or
+#'   [min, min + 2 * pi) is used as accepted range.
+#'   When set `TRUE`, (min, min + 360] or (min, min + 2 * pi]
+#'   is used instead.
+#' @param rad A logical. Whether the values are in radian unit or not.
+#'
+#' @return Numerics.
+#'   Angular values adjusted to fit within the given range.
+#'
+#' @examples
+#'   d <- c(-120, -45, 0, 90, 180, 360)
+#'   r <- d / pi * 180
+#'   wrapdeg(d)
+#'   wrapdeg(d, -180)
+#'   wrapdeg(d, -180, leftopen = TRUE)
+#'   wrapdeg(r, -pi, rad = TRUE)
+#'
+#' @keywords math
+#'
+#' @export
+
 wrapdeg <- function(
 
 	deg,
-	left = 0,
+	min = 0,
 	leftopen = FALSE, # default [min, max)
 	rad = FALSE
 
 ) {
 
 unit <- ifelse(rad, 2 * pi, 360)
+deg <- deg - min
 deg <- deg - (deg %/% unit) * unit
 if (leftopen) {
 	deg[deg == 0] <- unit
 }
-deg <- deg + left
+deg <- deg + min
 
 return(deg)
 }
---
+++ tests/testthat/test_deg.R
@@ -0,0 +1,20 @@
+context("Angular Value Handling")
+library(htb)
+
+d <- c(-180, -90, 0, 45, 180, 360)
+
+test_that("wrapdeg() returns right-open degrees by default", {
+	expect_equal(wrapdeg(d, 0), c(180, 270, 0, 45, 180, 0))
+	expect_equal(wrapdeg(d, -180), c(-180, -90, 0, 45, -180, 0))
+})
+
+test_that("Minimal value is excluded from the left-open range", {
+	expect_equal(wrapdeg(d, 0, leftopen = TRUE), c(180, 270, 360, 45, 180, 360))
+})
+
+r <- c(-pi, 0, pi, 2 * pi)
+
+test_that("wrapdeg() also accepts radian values", {
+	expect_equal(wrapdeg(r, rad = TRUE), c(pi, 0, pi, 0))
+})
+
